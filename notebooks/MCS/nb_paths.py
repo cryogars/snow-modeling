cea026aa8025320b9f4700ec8668334e2664a1c1
Joachim Meyer <jmeyer@boisestate.edu>
2025-02-07 15:54:12 -0700

    Notebooks - Move MCS specific share helpers into folder
    Prepars analyzing more areas (e.g. Cameron Pass)

--- notebooks/MCS/nb_paths.py
+++ notebooks/MCS/nb_paths.py
@@ -1,1 +1,8 @@
-../nb_paths.py
+from pathlib import Path, PurePath
+
+GROUP_STORE = PurePath('/bsushare/hpmarshall-shared/jmeyer')
+SNOBAL_STORE = GROUP_STORE.joinpath('iSnobal')
+# Model project files
+DATA_DIR = SNOBAL_STORE.joinpath('project-data')
+# Model output files
+SNOBAL_DIR = SNOBAL_STORE.joinpath('MCS/isnobal')
